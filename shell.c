ee57eb0d705fe870c13b765948fc61182e3a3999
Maggie <mzhao3@stuy.edu>
2018-11-19 15:35:24 -0500

    x

---
+++ shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+
+int main() {
+  return 0;
+}
