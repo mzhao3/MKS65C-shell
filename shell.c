62410a116ec1ed5792775750aa91ba97c8bd0ab8
Kevinlyy <kevin@Kevins-Laptop.localdomain>
2018-12-02 18:09:16 -0500

    recognizes >, >>, <, |

--- shell.c
+++ shell.c
@@ -34,8 +34,8 @@ void noSpaacepls(char * oboi){
   int i = 0;
   int c = 0;
   for(; oboi[i]; ++i)
-    if(((i > 0 && !isspace(oboi[i-1])) || !isspace(oboi[i])))
-      oboi[c++]= oboi[i];
+  if(((i > 0 && !isspace(oboi[i-1])) || !isspace(oboi[i])))
+  oboi[c++]= oboi[i];
   oboi[c] = '\0';
 
   int b = strlen(oboi)-1;
@@ -47,75 +47,120 @@ void noSpaacepls(char * oboi){
     if(oboi[b] != ' '){
       break;
     }
-
   }
-
 }
-//[what the heckk;what      ]
-//[what the heckk;what]
-int main(int argc, char *argv[]) {
-  int x = getpid();
-  char * user = getpwuid(getuid()) -> pw_name;
-  char host[64];
-  gethostname(host,64);
-  char cwd[PATH_MAX];
-  while(1) {
-    char path[100];
-    if(getcwd(cwd,sizeof(cwd))!=NULL){
-      printf("%s@%s:%s$ ",user,host,cwd);
+
+//check for >, >>, <, | return an int based on which one
+int checkfordirections(char * line){
+  int i = 0;
+  for(;i < strlen(line);i++){
+    if(line[i] == '>' && line[i+=1] == '>'){
+      return 2;
     }
-    else{
-      printf("error");
-    }
-
-    fgets(path,100,stdin );
-    path[strlen(path)-1] = '\0';
-    //noSpaacepls(path);
-
-    char *s1 = path;
-    char **separgs = parse_args(s1, ";");
-
-
-    int i;
-    for(i = 0; separgs[i];i++){
-      noSpaacepls(separgs[i]);
-      char ** args = parse_args(separgs[i], " " );
-
-
-      if (!strcmp(args[0], "exit")) {
-        printf("Exiting shell...\n");
-        exit(0);
-      }
-
-      if (!strcmp(args[0], "cd")) {
-        char * path2 = args[1];
-        //printf("%s\n", path);
-        //if (path2 == NULL)
-        //path2 = ".";
-        //printf("%s\n", path);
-        int err = chdir(path2);
-        //printf("%d\n", err);
-        if (err == -1)
-        printf("%s\n", strerror(errno));
-      }
-
-      int f = fork();
-
-      if(f){
-        wait(NULL);
-      }
-      if(!f && strcmp(args[0], "cd")){
-        execvp(args[0], args);
-        if(errno ==2){
-          printf("%s: command not found \n",args[0]);
-        }
-
-      }
-      free(args);
-    }
-
   }
-
-  //}
+  for(int i = 0;line[i];i++){
+    if(line[i] == '>'){
+      return 1;
+    }
+    if(line[i] == '<'){
+      return 3;
+    }
+    if(line[i] == '|'){
+      return 4;
+    }
+  }
   return 0;
 }
+
+ int main(int argc, char *argv[]) {
+   int x = getpid();
+   char * user = getpwuid(getuid()) -> pw_name;
+   char host[64];
+   gethostname(host,64);
+   char cwd[PATH_MAX];
+   while(1) {
+     char path[100];
+     if(getcwd(cwd,sizeof(cwd))!=NULL){
+       printf("%s@%s:%s$ ",user,host,cwd);
+     }
+     else{
+       printf("error");
+     }
+
+     fgets(path,100,stdin );
+     path[strlen(path)-1] = '\0';
+
+     char *s1 = path;
+     char **separgs = parse_args(s1, ";");
+
+
+     int i;
+     for(i = 0; separgs[i];i++){
+       noSpaacepls(separgs[i]);
+       int s;
+       s = checkfordirections(separgs[i]);
+
+
+
+       // >
+       if(s == 1){
+         printf("basic redirect \n");
+         
+       }
+
+
+       // >>
+       if(s == 2){
+         printf("temp 2\n");
+       }
+       //<
+       if(s == 3){
+         printf("temp 3\n");
+       }
+       // |
+       if(s == 4){
+         printf("temp 4\n");
+       }
+
+       //execute regular commands
+       if(s == 0){
+         char ** args = parse_args(separgs[i], " " );
+
+         if (!strcmp(args[0], "exit")) {
+           printf("Exiting shell...\n");
+           exit(0);
+         }
+
+         if (!strcmp(args[0], "cd")) {
+           char * path2 = args[1];
+           //printf("%s\n", path);
+           //if (path2 == NULL)
+           //path2 = ".";
+           //printf("%s\n", path);
+           int err = chdir(path2);
+           //printf("%d\n", err);
+           if (err == -1)
+           printf("%s\n", strerror(errno));
+         }
+
+         int f = fork();
+
+         if(f){
+           wait(NULL);
+         }
+         if(!f && strcmp(args[0], "cd")){
+           execvp(args[0], args);
+           if(errno ==2){
+             printf("%s: command not found \n",args[0]);
+           }
+
+         }
+         free(args);
+       }
+
+     }
+   }
+
+
+   return 0;
+ }
